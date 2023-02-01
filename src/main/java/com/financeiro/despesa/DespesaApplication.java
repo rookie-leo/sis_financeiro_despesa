7ceae72a206dd489351fe35290b859ef4ee343fb
rookie-leo <proleonarpa@gmail.com>
2023-02-01 10:38:13 -0300

    initial project setup

---
+++ src/main/java/com/financeiro/despesa/DespesaApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.despesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financeiro/despesa/DespesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.despesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
